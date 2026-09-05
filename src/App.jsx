b432d27d99b21de20eef8d56c161aa7f2661dfd4
Fasiullah131 <fasihullah.lki@gmail.com>
2026-09-05 14:00:25 +0500

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="landing-page-container">
+      <h1>Welcome to Paradise Nursery</h1>
+      <button>Get Started</button>
+    </div>
+  )
+}
+export default App
