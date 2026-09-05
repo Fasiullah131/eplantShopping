a0e2d95b141abf232ef9da25906cc81c3f06f0ce
Fasiullah131 <fasihullah.lki@gmail.com>
2026-09-05 13:59:21 +0500

    Create AboutUs.jsx

---
+++ src/AboutUs.jsx
@@ -0,0 +1,10 @@
+function AboutUs() {
+  return (
+    <div className="about-us-container">
+      <h1>About Paradise Nursery</h1>
+      <p>We provide best quality plants for your home and garden since 2020.</p>
+      <p>Our Mission: Make every home green and beautiful.</p>
+    </div>
+  )
+}
+export default AboutUs
